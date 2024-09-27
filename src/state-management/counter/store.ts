41b4500e7cd6a5a7221af73faa05961849486f13
Lijuan Wang <2738042w@student.gla.ac.uk>
2024-09-28 00:10:52 +0100

    add zustand devtools

--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -1,4 +1,5 @@
 import {create} from 'zustand'
+import { mountStoreDevtool } from 'simple-zustand-devtools'
 
 interface CounterStore { 
     counter: number;
@@ -14,7 +15,7 @@ interface CounterStore {
     reset: () => set(() => ({ max: 10 }))
   }));
   
-//   if (process.env.NODE_ENV === 'development')
-//     mountStoreDevtool('Counter Store', useCounterStore);
+  if (process.env.NODE_ENV === 'development')
+    mountStoreDevtool('Counter Store', useCounterStore);
   
   export default useCounterStore;
